fafb7cdf970c1713792447d29089a1791decf334
Xandre99ghost@gmail.com <BNCandre99gho$t>
2019-12-12 11:18:03 +0100

    v. 0.1.1 *Progettazione* -> Definizione dei TODO e dei JavaDoc per il package FP_Queue

--- src/FP_Queue/Queue_File.java
+++ src/FP_Queue/Queue_File.java
@@ -1,5 +1,18 @@
 package FP_Queue;
 
+/**
+ * 
+ * @author Tirelli Andrea
+ * Classe che definisce la struttura dati Coda
+ * La Coda definisce la struttura per lo store dei file selezionati dall'user
+ * La Coda definisce l'interfaccia per il quale il software può accedere ai file in coda
+ * La Coda definisce anche le regole per il quale i file vengono pushati e poppoati dalla coda 
+ *
+ */
+
 public class Queue_File {
+	// TODO Struttura della coda per i file
+	
+	// TODO Interfaccia per l'accesso dei file in coda
 
 }
